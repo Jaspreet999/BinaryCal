dcef2dceb9d326b49915f4e21af4320cf90e492a
jaspreetsinghngh@gmail.com <jaspreetsinghngh@gmail.com>
2022-08-10 17:45:16 +0530

    updated commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const operation = document.getElementById('operation')
+const firstValue = document.getElementById('firstValue')
+const secondValue = document.getElementById('secondValue')
+let button = document.getElementById('submit')
+const error = document.getElementById('errorField')
+const result = document.getElementById('resultDisplay')
+
+function negationOfData(data){
+    if(data == 0) return 1
+    if(data == 1) return 0
+
+   let binary =""
+    while(data !=0){
+        if(data%2 == 0){
+            binary = "1" + binary
+        }else{
+            binary = "0" +binary
+        }
+
+        data = parseInt(data/2)
+    }
+    // console.log(binary)
+    // convert to negation
+    c = 0
+    size = binary.length -1
+    for(let i =0;i<binary.length;i++){
+        if(binary[i] == '1'){
+            c = c + Math.pow(2,size)
+            console.log(c)
+        }
+        size = size -1
+        console.log(size)
+    }
+     
+    return c
+
+}
+
+button.addEventListener('click',validation)
+
+function validation(){
+   
+    if(operation.value == "negation"){
+        if(firstValue.value.length != 0){
+                       
+            result.textContent = "Result :"+ (negationOfData(parseInt(firstValue.value)))
+        }else{
+            alert("please enter first field for negation")
+        }    
+        
+    }else{
+    
+        if(firstValue.value.length ==0 || secondValue.value.length ==0){
+            alert("please enter both the field")
+        }else{
+                
+                doCalculation()
+                
+        }
+    }    
+}
+
+function doCalculation(){
+    console.log()
+    let a = parseInt(firstValue.value)
+    let b = parseInt(secondValue.value)
+
+    if(operation.value == 'or'){
+
+        result.textContent = "Result :"+ (a|b)
+
+    }else if(operation.value == 'and'){
+        
+        result.textContent = "Result :"+ (a&b)
+
+    }else if(operation.value == 'xor'){
+        
+        result.textContent = "Result :"+ (a^b)
+
+    }else if(operation.value == 'nand'){
+        
+        result.textContent = "Result :"+ negationOfData(a&b)
+
+    }else if(operation.value == 'nor'){
+        
+        result.textContent = "Result :"+ negationOfData(a|b)
+
+    }else if(operation.value == 'xnor'){
+        
+        result.textContent = "Result :"+ negationOfData(a^b)
+
+    } 
+}
