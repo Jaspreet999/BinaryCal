8d302ace24d031e02d228dcf0957969308da898b
jaspreetsinghngh@gmail.com <jaspreetsinghngh@gmail.com>
2022-08-15 18:19:44 +0530

    update range of input

--- index.js
+++ index.js
@@ -35,15 +35,28 @@ function negationOfData(data){
     return c
 
 }
+function checkValue(a){
+    if(a<100000 && a >(-100000)){
+        return true
+    }
+
+    return false
+}
 
 button.addEventListener('click',validation)
 
 function validation(){
    
     if(operation.value == "negation"){
-        if(firstValue.value.length != 0){
-                       
-            result.textContent = "Result :"+ (negationOfData(parseInt(firstValue.value)))
+        if(firstValue.value.length != 0 && firstValue){
+           
+            if(checkValue(parseInt(firstValue.value))){
+                result.textContent = "Result :"+ (negationOfData(parseInt(firstValue.value)))
+            }else{
+                alert("please enter value less than 100000 and greater than -100000")
+            }
+            
+            
         }else{
             alert("please enter first field for negation")
         }    
@@ -53,8 +66,12 @@ function validation(){
         if(firstValue.value.length ==0 || secondValue.value.length ==0){
             alert("please enter both the field")
         }else{
+                if(checkValue(parseInt(firstValue.value)) && checkValue(parseInt(firstValue.value))){
+                    doCalculation()
+                }else{
+                    alert("please enter value less than 100000 and greater than -100000")
+                }
                 
-                doCalculation()
                 
         }
     }    
